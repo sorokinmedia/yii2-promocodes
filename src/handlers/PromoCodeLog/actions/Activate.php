67c9b34cc1368eedb9f3337f76cf7ea5aece8a6b
ma3obblu <Ma3oBblu@gmail.com>
2019-01-18 00:51:33 +0400

    try catch for activation

--- src/handlers/PromoCodeLog/actions/Activate.php
+++ src/handlers/PromoCodeLog/actions/Activate.php
@@ -20,7 +20,10 @@ class Activate extends AbstractAction
             $operation_id = (new PromoCodeHandler($this->promo_code_log->promoCode))->activate($this->promo_code_log->user);
             return $this->promo_code_log->setActivated($operation_id);
         } catch (\Exception $exception){
-            throw new \Exception(\Yii::t('app', 'Ошибка при активации промокода'));
+            throw new \Exception(\Yii::t('app', 'Ошибка при активации промокода, лог:{log_id}, {error}', [
+                'log_id' => $this->promo_code_log->id,
+                'error' => $exception->getMessage()
+            ]));
         }
     }
 }
