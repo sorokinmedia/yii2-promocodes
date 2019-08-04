f6d7b564b0949af87806182e7a1eecdd7112f495
ma3obblu <Ma3oBblu@gmail.com>
2019-08-04 18:21:22 +0400

    [fix] add PromoCodeLogExistException

---
+++ src/exceptions/PromoCodeLogExistException.php
@@ -0,0 +1,19 @@
+<?php
+namespace sorokinmedia\promocodes\exceptions;
+
+use yii\db\Exception;
+
+/**
+ * Class PromoCodeLogExistException
+ * @package sorokinmedia\promocodes\exceptions
+ */
+class PromoCodeLogExistException extends Exception
+{
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return 'PromoCodeLog Exist Exception';
+    }
+}
